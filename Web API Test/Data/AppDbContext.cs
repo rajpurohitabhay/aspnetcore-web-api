48db9cfdaad22aef1b620de6beb73a5d69aec0c9
Abhaysingh Rajpurohit <abu.cool999@gmail.com>
2022-07-21 23:59:48 +0530

    Added Database Connection

---
+++ Web API Test/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web_API_Test.Data.Models;
+
+namespace Web_API_Test.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
