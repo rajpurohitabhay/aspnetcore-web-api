7a578a7e59765d1adf98dae9550ca7b718d33825
Abhaysingh Rajpurohit <abu.cool999@gmail.com>
2022-07-27 22:45:15 +0530

    Data Migration and JWT Token Example Files

---
+++ SuperHeroAPI/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿namespace SuperHeroAPI.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+    }
+}
